eb052f67bbc8576c760181ad4a90eff360b0cae2
Kiantis <kiantisg@gmail.com>
2023-07-11 23:51:01 +0200

    refactor for reuse

--- src/user_lister.rs
+++ src/user_lister.rs
@@ -50,21 +50,30 @@ mod tests {
 
     #[test]
     fn test() {
+        assert_messages(vec![("JOINED", "User")], vec!["Users in session: User"]);
+    }
+
+    fn assert_messages(inputs: Vec<(&str, &str)>, outputs: Vec<&str>) {
         let messages = Arc::new(Mutex::new(Vec::new()));
         let mock = Arc::new(MemoryDiscord {
             messages: messages.clone(),
         });
         let lister = UserLister { discord: mock };
 
-        lister.json_message(
-            r#"
-        {
-            "type": "JOINED",
-            "userName": "User"
+        for (typ, msg) in inputs {
+            let msg = format!(
+                r#"
+            {{
+                "type": "{typ}",
+                "userName": "{msg}"
+            }}
+            "#
+            );
+            lister.json_message(msg.as_str());
         }
-        "#,
-        );
 
-        assert_eq!(messages.lock().unwrap()[0], "Users in session: User");
+        for (idx, msg) in outputs.iter().enumerate() {
+            assert_eq!(&messages.lock().unwrap()[idx].as_str(), msg);
+        }
     }
 }
